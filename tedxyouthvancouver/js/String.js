5949182a1541415c3cd887e3cbb41b336ecb48f1
Salehen Rahman <salehen.rahman@gmail.com>
2011-08-11 17:58:31 -0700

    Fixed some CSS.

--- tedxyouthvancouver/js/String.js
+++ tedxyouthvancouver/js/String.js
@@ -19,21 +19,21 @@
 		 // Replace all instances of all plain-text URL with
 		 // a hyperlink.
 		 replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
-				'<a href="$1" target="_blank">$1</a>')
+				'<a href="$1">$1</a>')
 		 
 		 // Replace all instances of of a mention with a URL
 		 // to the mentioned username.
 		.replace(/(\B@(\w|[a-b]|\d|_){1,15})/gi, 
 		function(u) {
 			var username = u.replace('@', '');
-			return '<a href="http://twitter.com/' + username + '" target="_blank">' + u + '</a>';
+			return '<a href="http://twitter.com/' + username + '">' + u + '</a>';
 		})
 		
 		 // Replace all hashtags with a URL to the hashtag's
 		 // search page.
 		.replace(/(\B#(\w|[a-b]|\d|_)+)/gi, function(h) {
 			var hashtag = h.replace('#', '');
-			return '<a href="http://twitter.com/search?q=%23' + hashtag + '" target="_blank">' + h +'</a>';
+			return '<a href="http://twitter.com/search?q=%23' + hashtag + '">' + h +'</a>';
 		});
 	};
 })(this, document)
